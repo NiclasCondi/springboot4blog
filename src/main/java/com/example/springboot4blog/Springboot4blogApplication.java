c6aa55c1970f8d3f07710ae99e771c25818a92b8
WXSK-天涯 <15766005371@163.com>
2018-06-04 13:54:12 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot4blog/Springboot4blogApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot4blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4blogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot4blogApplication.class, args);
+    }
+}
